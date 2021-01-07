e6ead0dd1a12242406f1f4ab8daee3cff582d1a1
Janek <spikedevelopment@gmail.com>
2021-01-07 22:28:16 +0100

    final

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 console.log('Working!')
+
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+
+// prompt user to select media stream
+
+async function selectMediaStream() {
+    try {
+
+       const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+       videoElement.srcObject = mediaStream;
+       videoElement.onloadedmetadata = _ => {
+           videoElement.play();
+       }
+
+    } catch (error) {
+        console.log('Error selecting media stream', error);
+    }
+}
+
+
+button.addEventListener('click', async _ => {
+    button.disabled = true;
+    await videoElement.requestPictureInPicture();
+    button.disabled = false;
+})
+
+
+// On load
+selectMediaStream();
